afa5b371b996656e7268a8fd28fc84316d1f64e8
gaoyang <xz@lgysh.cn>
2026-03-02 11:37:01 +0800

    Revert "update my config."
    This reverts commit bb2246b021e19fc544db021cda96196e2ff357f2.

--- lib/get-block-value.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 从 recordMap 中的 record（如 block[id]、notion_user[id]）取出 .value。
- * notion-utils 新版本移除了 getBlockValue 导出，故在本地实现。
- */
-export function getBlockValue<T>(
-  record: { value: T } | undefined
-): T | undefined {
-  return record?.value
-}
